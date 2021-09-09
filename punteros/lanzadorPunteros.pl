6d43ed57580f9fc3113e74339ce9c18035ce05bf
Neftcode <arevalocdev@outlook.com>
2021-09-09 02:45:49 -0500

    comentario de instrucciones

--- punteros/lanzadorPunteros.pl
+++ punteros/lanzadorPunteros.pl
@@ -7,7 +7,7 @@
 $repeticiones = 30; #30 repeticiones por tamaño de matriz
 @ejecutables = ("matricesApp");
 @matrizSize = ("3", "4", "5", "10", "100", "400", "800", "1000", "2500", "5000");
-$path = "/home/{path}/Algorithms/punteros/"; #reemplazar {path} por la ruta donde se encuentra ubicado el proyecto (ej: /home/pedro/paralela/Algorithms/punteros/)
+$path = "/home/{path}/Algorithms/punteros/"; #reemplazar {path} por la ruta donde se encuentra ubicado el proyecto (ej: /home/pedro/paralela/Algorithms/punteros/) IMPORTANTE: la ruta debe finalizar con / al final
 
 foreach $exe (@ejecutables) {
 	foreach $size (@matrizSize) {
